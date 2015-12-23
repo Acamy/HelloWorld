a0dd75bd5adccbcb4c6fe10e23ce0b74de3e3443
He Baohua <hebaohua@hust.edu.cn>
2015-12-23 09:53:24 +0800

    listbox

---
+++ Winform基础/1223.listbox实现播放选中图片/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _1223.listbox实现播放选中图片
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] path = Directory.GetFiles(@"E:\Pictures");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+            for (int i = 0; i < path.Length; i++)
+            {
+                string name=Path.GetFileName(path[i]);
+                listBox1.Items.Add(name);
+            }
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = Image.FromFile(path[listBox1.SelectedIndex]);
+        }
+    }
+}
